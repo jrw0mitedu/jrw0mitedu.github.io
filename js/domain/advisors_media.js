d581fb11287c8d52445205f21bf759792ef82641
jrw0mitedu <jrw0mitedu>
2018-07-05 15:17:33 -0400

    fashion 31

--- js/domain/advisors_media.js
+++ js/domain/advisors_media.js
@@ -3,7 +3,7 @@
 
 var banner = {
     title       : 'Media Advisors',
-    description : gdc.advisorsDescription + gdc.advisorsButtons,
+    description :  gdc.advisorsButtons,
     image       : 'img/data/bokeh.jpg'
 };
 
@@ -12,16 +12,12 @@ var banner = {
 
 var features = [];
 features.push({
-    title       : 'Laris Vorontsova',
+    title       : 'Lora',
     description : '<div><p>Larisa has an MBA from Middlesex University and honed her design skills in Russia. She was responsible for the design of a cinema complex in Ufa. </p></div>',
     image       : 'img/people/lora4.png'
 });
 
-features.push({
-    title       : 'Tiffany Shlain',
-    description : '<div><p>Honored by Newsweek as one of the “Women Shaping the 21st Century,” Tiffany Shlain is a filmmaker, artist, founder of The Webby Awards, and co-founder of the International Academy of Digital Arts and Sciences. Tiffany founded The Webby Awards in 1996 and was creative director and CEO for nearly a decade, establishing it into a global organization honoring the best of the Internet. The Webbys receive over 10,000 entries annually and are presented annually in NYC. She co-founded The International Academy of Digital Arts & Sciences, today a 550-person organization, and served as the on-air Internet expert for Good Morning America from 2000 to 2003. Her work with The Webbys has been profiled on The New York Times, The BBC, ABC, MTV, CNN, The Hollywood Reporter, Vanity Fair, and NPR.</p></div>',
-    image       : 'img/people/advisors/tiffany.jpg'
-});
+
 
 
 
