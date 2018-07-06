a580cbc96ee2a22b2e7b05cce596b153def0dd03
jrw0mitedu <jrw0mitedu>
2018-07-06 08:20:27 -0400

    fashion 74

--- js/domain/costs.js
+++ js/domain/costs.js
@@ -2,23 +2,61 @@
 // ----------------------------------
 
 var banner = {
-    title       : 'Costs',
-    description : 'Planning the Wardrobe (half day) £350<br> Full day Shopping £600 <br>Lora will guide you to meet your goals and stay within your budget...<br>Your new image awaits you. <div><a href="ar.html" class="badge badge-info">More Information ...</a></div>',
-    image       : 'images/02.jpg',
-    linkIsVideo : true,    
-    link        : 'img/data/bokeh.jpg'
+    title       : 'Initial Recommendation',
+    description : "Wardrobe review and plann (half day) £350<br> Full day shopping in London or city of your choice £600 <br> Lora will help you rationalise your wardrobe and develop a plan to fill the missing spaces. Then we recommend a day in London to shop. Lora will guide you so you'll meet your goals and stay within your budget...<br> If you want additional consults on fashion or on fitness and well-being then Lora is available. <br> Rate per hour £120<br> ",
+    image       : 'images/london.jpg',
 };
 
 // the domain videos
 // ----------------------------------
 
 var boxes = [];
+
 boxes.push({
-    title       : 'Cost Per Hour',
-    subtitle    : '£120',
+    title       : 'Anytime',
+    subtitle    : '',
     description : '',
     url         : '',
     video       : false,
-    thumbnail   : 'images/car.jpg'
+    thumbnail   : 'images/Fashion/china1.jpg'
 });
-
+boxes.push({
+    title       : 'Informal',
+    subtitle    : '',
+    description : '',
+    url         : '',
+    video       : false,
+    thumbnail   : 'images/Fashion/short1.jpg'
+});
+boxes.push({
+    title       : 'Stylish',
+    subtitle    : '',
+    description : '',
+    url         : '',
+    video       : false,
+    thumbnail   : 'images/Fashion/china2.jpg'
+});
+boxes.push({
+    title       : 'Informal',
+    subtitle    : '',
+    description : '',
+    url         : '',
+    video       : false,
+    thumbnail   : 'images/Fashion/shapely2.jpg'
+});
+boxes.push({
+    title       : 'Fun',
+    subtitle    : '',
+    description : '',
+    url         : '',
+    video       : false,
+    thumbnail   : 'images/Fashion/fun1.jpg'
+});
+boxes.push({
+    title       : 'Evening',
+    subtitle    : '',
+    description : '',
+    url         : '',
+    video       : false,
+    thumbnail   : 'images/Fashion/shapely4.jpg'
+});
