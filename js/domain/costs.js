052f172c307e5af0f10cc7c2ef7cd17ff2bcf0f0
jrw0mitedu <jrw0mitedu>
2018-07-05 20:58:56 -0400

    fashion 61

---
+++ js/domain/costs.js
@@ -0,0 +1,24 @@
+// the domain banner 
+// ----------------------------------
+
+var banner = {
+    title       : 'Costs',
+    description : 'Planning the Wardrobe (half day) £350<br> Full day Shopping £600 <br>Lora will guide you to meet your goals and stay within your budget...<br>Your new image awaits you. <div><a href="ar.html" class="badge badge-info">More Information ...</a></div>',
+    image       : 'images/02.jpg',
+    linkIsVideo : true,    
+    link        : 'img/data/bokeh.jpg'
+};
+
+// the domain videos
+// ----------------------------------
+
+var boxes = [];
+boxes.push({
+    title       : 'Cost Per Hour',
+    subtitle    : '£120',
+    description : '',
+    url         : '',
+    video       : false,
+    thumbnail   : 'images/car.jpg'
+});
+
