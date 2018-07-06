b34fb8b45d556f130273bbae168e708a249abe46
jrw0mitedu <jrw0mitedu>
2018-07-06 09:00:46 -0400

    fashion 75

--- js/domain/fashion.js
+++ js/domain/fashion.js
@@ -14,78 +14,81 @@ var banner = {
 
 var boxes = [];
 boxes.push({
-    title       : 'Going to the dance',
-    subtitle    : 'Formal dance',
+    title       : 'Dance Dress',
+    subtitle    : '',
     description : '',
     url         : 'https://www.youtube.com/embed/pnk-EawgbrA',
     video       : true,
-    thumbnail   : 'images/Fashion/lora2.jpg'    
+    thumbnail   : 'images/Fashion/lora4.jpg'    
 });
 boxes.push({
     title       : 'Staying abroad',
-    subtitle    : 'Walk about',
+    subtitle    : '',
     description : '',
     url         : 'https://www.youtube.com/embed/Kypp0XhcImU',
     video       : true,
-    thumbnail   : 'images/Fashion/elevator2.jpg'
+    thumbnail   : 'images/Fashion/elevator4.jpg'
 });
 boxes.push({
     title       : 'Late night',
-    subtitle    : 'Drama',
+    subtitle    : '',
     description : '',
     url         : 'https://www.youtube.com/embed/Kypp0XhcImU',
     video       : true,
     thumbnail   : 'images/Fashion/nightLife1.jpg'
 });
 boxes.push({
-    title       : 'City',
-    subtitle    : 'Dress for dinner',
+    title       : 'Dinner',
+    subtitle    : '',
     description : '',
     thumbnail   : 'images/Fashion/dinner.png'
 });
 boxes.push({
-    title       : 'Dinner in the City',
-    subtitle    : 'walking to dinner',
+    title       : 'Evening at home',
+    subtitle    : '',
+    description : '',
+    thumbnail   : 'images/Fashion/fireplace.jpg'
+});
+boxes.push({
+    title       : 'Shopping',
+    subtitle    : '',
     description : '',
     thumbnail   : 'images/Fashion/red.jpg'
 });
+
 boxes.push({
-    title       : 'Dinner in hotel',
+    title       : 'Special occasion',
+    subtitle    : '',
+    description : '',
+    thumbnail   : 'images/Fashion/HK1.jpg'    
+});
+boxes.push({
+    title       : 'Flying',
+    subtitle    : '',
+    description : '',
+    url         : 'https://www.youtube.com/embed/pnk-EawgbrA',
+    video       : true,
+    thumbnail   : 'images/Fashion/travel.jpg'    
+});
+boxes.push({
+    title       : 'Summer Dress',
+    subtitle    : '',
+    description : '',
+    thumbnail   : 'images/Fashion/summer.jpg'
+});
+
+boxes.push({
+    title       : 'Weekend at home',
+    subtitle    : '',
+    description : '',
+    thumbnail   : 'images/Fashion/house.jpg'
+});
+boxes.push({
+    title       : 'Fashion show',
     subtitle    : '',
     description : '',
     url         : 'https://www.youtube.com/embed/Kypp0XhcImU',
     video       : true,
     thumbnail   : 'images/Fashion/dinner1.jpg'
 });
-boxes.push({
-    title       : 'Special occasion',
-    subtitle    : 'Making a statement',
-    description : '',
-    thumbnail   : 'images/Fashion/HK1.jpg'    
-});
-boxes.push({
-    title       : 'Summer Dress',
-    subtitle    : 'Being casual',
-    description : '',
-    thumbnail   : 'images/Fashion/summer.jpg'
-});
-boxes.push({
-    title       : 'Flying',
-    subtitle    : 'Dress to fly',
-    description : 'Situational awareness.',
-    url         : 'https://www.youtube.com/embed/pnk-EawgbrA',
-    video       : true,
-    thumbnail   : 'images/Fashion/travel.jpg'    
-});
-boxes.push({
-    title       : 'Weekend at home',
-    subtitle    : 'Easy to wear',
-    description : '',
-    thumbnail   : 'images/Fashion/house.jpg'
-});
-boxes.push({
-    title       : 'Evening at home',
-    subtitle    : 'With wine perhaps',
-    description : '',
-    thumbnail   : 'images/Fashion/fireplace.jpg'
-});
+
