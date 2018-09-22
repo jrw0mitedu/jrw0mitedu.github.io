0bb462692941051005d21cb6f93b541d1c6130fb
jrw0mitedu <jrw0mitedu>
2018-09-22 13:33:27 -0400

    cost reduced

--- js/domain/costs.js
+++ js/domain/costs.js
@@ -4,7 +4,7 @@
 var banner = {
     title       : 'Initial Recommendation-2 Step Plan ',
     subtitle    : '',
-    description : "Step 1 - Wardrobe review and planning (half day) £350<br> Step 2 - Full day shopping in London or city of your choice £600 <br> Lora will help you rationalise your wardrobe and develop a plan to fill the missing spaces. Then we recommend a day shopping in London. Lora will guide you so you'll meet your goals and stay within your budget...<br> If you want additional consults on fashion or on fitness and well-being then Lora is available. <br> Rate per hour £120<br>Contact: <h2> tel. 799 080 8003, larisa.vorontsofa@gmail.com</h2>",
+    description : "Step 1 - Wardrobe review and planning (half day) £150<br> Step 2 - Full day shopping in London or city of your choice £300 <br> Lora will help you rationalise your wardrobe and develop a plan to fill the missing spaces. Then we recommend a day shopping in London. Lora will guide you so you'll meet your goals and stay within your budget...<br> If you want additional consults on fashion or on fitness and well-being then Lora is available. <br> Rate per hour £120<br>Contact: <h2> tel. 799 080 8003, larisa.vorontsofa@gmail.com</h2>",
     image       : 'images/london.jpg',
 };
 
